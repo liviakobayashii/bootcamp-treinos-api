b262e06fa3bb267f9312f29bcbd6d02b977775a1
Livia Kobayashi <liviakmmartins@gmail.com>
2026-03-20 19:17:10 -0300

    chore: configure trusted origins using the `FRONTEND_URL` environment variable instead

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -13,7 +13,7 @@ const prisma = new PrismaClient({
 
 export const auth = betterAuth({
   baseURL: process.env.BETTER_AUTH_URL,
-  trustedOrigins: ["http://localhost:3000"],
+  trustedOrigins: [process.env.FRONTEND_URL as string],
   socialProviders: {
     google: { 
         prompt: "select_account",
